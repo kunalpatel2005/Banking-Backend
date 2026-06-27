09acfc87568ab0f1c2a03f5e5bd4685f8310e129
kunalpatel2005 <kunalpatelklv@gmail.com>
2026-06-27 14:11:22 +0530

    Add root route to confirm server is running

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const transactionrouter = require("./routes/transaction.routes")
 const app = express();
 app.use(express.json())
 app.use(cookieparser())
+app.get("/", (req, res) => {
+    res.send("server is running")
+})
 app.use("/api/auth",authRouter)   //jo bhi req "/api/auth" ispe hit hofi vo authrouter pe jayegi
 app.use("/api/accounts",accountrouter)
 app.use("/api/transaction",transactionrouter)
